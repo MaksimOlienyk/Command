d2e499d51395ac64b6f28bc15f9950f739a7f75e
maljaethereal <121117752+MaksimOlienyk@users.noreply.github.com>
2025-10-19 18:51:54 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿interface ICommand { void Execute(); }
+
+class Receiver { public void Action() => Console.WriteLine("Action performed"); }
+
+class ConcreteCommand : ICommand
+{
+    private Receiver _receiver;
+    public ConcreteCommand(Receiver r) { _receiver = r; }
+    public void Execute() => _receiver.Action();
+}
+
+class Invoker { private ICommand _cmd; public void SetCommand(ICommand c) => _cmd = c; public void Run() => _cmd.Execute(); }
+
+class Program
+{
+    static void Main()
+    {
+        var invoker = new Invoker();
+        invoker.SetCommand(new ConcreteCommand(new Receiver()));
+        invoker.Run();
+    }
+}
+
